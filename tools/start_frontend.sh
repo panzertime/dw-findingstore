e4863b0ec58d057a300800dd4e5264a9060e1b1e
RT Hatfield <arthur_hatfield@homedepot.com>
2018-12-12 14:25:17 -0500

    start_frontend now works properly

--- tools/start_frontend.sh
+++ tools/start_frontend.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# set ES_HOST and ES_PORT
-export ES_HOST=localhost
-export ES_PORT=9200
+source ../virtualenv/bin/activate
 
-../virtualenv/bin/activate
+# set ES_HOST and ES_PORT
+set ES_HOST="localhost"
+set ES_PORT="9200"
+
+# set FRONTEND_PORT
+set FRONTEND_PORT="5000"
+
 python ../frontend/app.py
