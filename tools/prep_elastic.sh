233792b71a0812c64f4ab8bda7154407e1dba56a
RT Hatfield <arthur_hatfield@homedepot.com>
2018-12-13 14:27:37 -0500

    Created the Elastic document mapping. Made a tool to "initialize" Elastic for Findingstore

---
+++ tools/prep_elastic.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# set variables
+export ELASTIC_HOST="localhost"
+export ELASTIC_PORT="9200"
+
+curl -X PUT "$ELASTIC_HOST:$ELASTIC_PORT/findingstore_index" -H 'Content-Type: application/json' -d'
+{
+    "mappings": {
+        "finding_card": {
+            "properties": {
+                "url": {
+                    "type": "text"
+                },
+                "forumname": {
+                    "type": "text"
+                },
+                "vendorname": {
+                    "type": "text"
+                },
+                "category": {
+                    "type": "text"
+                },
+                "keywords": {
+                    "type": "keyword"
+                },
+                "summary": {
+                    "type": "text"
+                },
+                "text_evidences": {
+                    "type": "text"
+                },
+                "binary_evidences": {
+                    "type": "binary",
+                    "store": true
+                },
+                "created": {
+                    "type": "date"
+                }
+            }
+        }
+    }
+}
+'
